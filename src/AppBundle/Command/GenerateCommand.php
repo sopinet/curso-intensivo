49cc377adba88fb775c0a62799062d993c818f4b
hidabe <fernando@sopinet.com>
2017-04-23 17:49:21 +0200

    added colors for output messages, and more messages and checks

--- src/AppBundle/Command/GenerateCommand.php
+++ src/AppBundle/Command/GenerateCommand.php
@@ -42,6 +42,8 @@ class GenerateCommand extends ContainerAwareCommand
             $format = self::FORMAT_PDF;
         }
 
+        $output->writeln("Vamos a intentar generar la carta número ".$cardId." en formato ".$format."...");
+
         /** @var EntityManager $entityManager */
         $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
         /** @var EntityRepository $repositoryCard */
@@ -49,22 +51,45 @@ class GenerateCommand extends ContainerAwareCommand
         /** @var Card $card */
         $card = $repositoryCard->findOneById($cardId);
 
-        $html = $this->getContainer()->get('templating')->render('previewCard.html.twig', array(
-            'cardNumber'  => $card->getId()
-        ));
-
-        $fs = new Filesystem();
-        if ($format == self::FORMAT_HTML) {
-            $fs->remove('card.html');
-            $fs->dumpFile('card.html', $html);
+        // Si la carta no existe
+        if (!$card instanceof Card) {
+            $output->writeln("<error>No se ha encontrado la carta especificada en la base de datos.</error>");
+        // Si la carta sí existe
         } else {
-            $fs->remove('card.pdf');
-            $this->getContainer()->get('knp_snappy.pdf')->generateFromHtml(
-                $html,
-                'card.pdf'
-            );
-        }
+            $output->writeln('<comment>Carta encontrada en la base de datos.</comment>');
+            $html = $this->getContainer()->get('templating')->render('previewCard.html.twig', array(
+                'cardNumber' => $card->getId()
+            ));
 
-        $output->writeln("Comando generate para cardId: ".$cardId. " y opción format: ".$format.". Ejecutado con éxito.");
+            $fs = new Filesystem();
+            // Asignamos el nombre del fichero a generar a una variable para poder trabajar con algunas comprobaciones
+            // de forma común para cualquier tipo de formato especificado.
+            $filenameForGenerate = 'card.' . $format;
+
+            // Si el fichero que vamos a generar ya existe lo eliminamos previamente
+            if ($fs->exists($filenameForGenerate)) {
+                $output->writeln("<comment>Ya existía un fichero " . $format . " con el mismo nombre, se ha eliminado.</comment>");
+                $fs->remove($filenameForGenerate);
+            }
+
+            // Ahora, según el formato especificado, se genera un HTML o un PDF
+            if ($format == self::FORMAT_HTML) {
+                $fs->dumpFile($filenameForGenerate, $html);
+            }
+            else {
+                $this->getContainer()->get('knp_snappy.pdf')->generateFromHtml(
+                    $html,
+                    $filenameForGenerate
+                );
+            }
+
+            // Se hace una comprobación final para indicar si el fichero existe, en cuyo caso entenderemos que
+            // éste se ha generado correctamente.
+            if ($fs->exists($filenameForGenerate)) {
+                $output->writeln('<info>El comando se ha generado con éxito y el fichero '.$format.' se ha generado.</info>');
+            } else {
+                $output->writeln('<error>Ha ocurrido un error y el fichero '.$format.' no se ha generado.</error>');
+            }
+        }
     }
 }
